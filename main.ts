890478d2c4d36964f71fed2b7c452d2b5e542055
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-09-28 13:14:02 -0400

    added a variable

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    counter += 1
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+})
+let counter = 0
+counter = 0
